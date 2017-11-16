e2132c644c9a9899590c52e2434a0df9a4d58f02
Subhajit Sahu <wolfram77@gmail.com>
2017-11-16 17:44:16 +0530

    :monkey_face: use wn1.7.1

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ const cp = require('child_process');
 
 
 // 1. settings
-var url = 'http://wordnetcode.princeton.edu/1.7/wn17.unix.tar.gz';
+var url = 'http://wordnetcode.princeton.edu/1.7.1/WordNet-1.7.1.tar.gz';
 var dest = 'index.tar.gz';
 var path = '.';
 
@@ -12,6 +12,10 @@ var path = '.';
 // 2. download and extract
 download([{url, dest}], {}).get((err) => {
   tar.extract({'file': dest}).then(() => {
-    cp.execSync('rm index.tar.gz');
+    cp.execSync(
+      'rm index.tar.gz && '+
+      'mv WordNet-1.7.1/* . && '+
+      'rmdir WordNet-1.7.1'
+    );
   });
 });
