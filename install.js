4831280ad033ecc9316e9fba587ab066bb88b164
Subhajit Sahu <wolfram77@gmail.com>
2017-11-16 17:39:39 +0530

    :monkey_face: use wn1.7

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ const cp = require('child_process');
 
 
 // 1. settings
-var url = 'http://wordnetcode.princeton.edu/1.6/wn16.unix.tar.gz';
+var url = 'http://wordnetcode.princeton.edu/1.7/wn17.unix.tar.gz';
 var dest = 'index.tar.gz';
 var path = '.';
 
@@ -12,10 +12,6 @@ var path = '.';
 // 2. download and extract
 download([{url, dest}], {}).get((err) => {
   tar.extract({'file': dest}).then(() => {
-    cp.execSync(
-      'rm index.tar.gz && '+
-      'mv wordnet-1.6/* . && '+
-      'rmdir wordnet-1.6'
-    );
+    cp.execSync('rm index.tar.gz');
   });
 });
