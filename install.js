ce15daa2b56c8a804c6cefc760d53cd3729d19e3
Subhajit Sahu <wolfram77@gmail.com>
2017-11-16 14:01:31 +0530

    :monkey_face: download and extract

--- install.js
+++ install.js
@@ -1,7 +1,15 @@
 const download = require('download-file');
+const unzip = require('unzip');
+const fs = require('fs');
 
+
+// 1. settings
 var url = 'http://wordnetcode.princeton.edu/1.5/wn15.zip';
 var filename = 'index.zip';
+var path = '.';
+
+
+// 2. download and extract
 download(url, {filename}, (err) => {
-  console.log('meow');
+  fs.createReadStream(filename).pipe(unzip.Extract({path}));
 });
